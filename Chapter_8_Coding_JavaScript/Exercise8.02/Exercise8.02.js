a80d89856dce45f9750f2196a5a953a38341172c
michaeljharrison <michael.j.harrison@outlook.com>
2020-08-20 16:51:43 +1000

    Fixed typos in the word "Exercise"

--- Chapter_8_Coding_JavaScript/Exercise8.02/Exercise8.02.js
+++ Chapter_8_Coding_JavaScript/Exercise8.02/Exercise8.02.js
@@ -12,8 +12,7 @@
 //// TOPIC B: "Simple Queries with the Driver."
 // Excercise 2: Simple Queries with the NodeJS Driver.
 const MongoClient = require('mongodb').MongoClient;
-const url =
-  'mongodb+srv://user:password@server-abcde.gcp.mongodb.net/test?retryWrites=true&w=majority';
+const url = process.env.ATLAS_URI;
 const client = new MongoClient(url);
 
 const databaseName = 'sample_mflix';
